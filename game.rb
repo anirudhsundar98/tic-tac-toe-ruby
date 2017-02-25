4dcd7921051bc15da451d075804e510dfc37ca05
Anirudh Sundar <anirudhsundar@hotmail.com>
2017-02-25 15:51:44 +0530

    Add win and draw condition checks

--- game.rb
+++ game.rb
@@ -1,29 +1,22 @@
-class User
+class Player
 	def turn
 		print "Move? - "
 		input = gets.strip
 		return input
 	end
-
-	def input_check(input)
-
-	end
 end
 
-class Bot
-	def turn
-		print "Move? - "
-		input = gets.strip
-		return input
+=begin
+	class Bot
+		def turn
+		end
 	end
-
-	# ...
-end
+=end
 
 class Game
 	@@players = {
-		'player1' => User.new,
-		'player2' => Bot.new
+		'player1' => Player.new,
+		'player2' => Player.new
 	}
 
 	def initialize
@@ -32,18 +25,21 @@ class Game
 			[" ", " ", " "],
 			[" ", " ", " "]
 		]
+		@turn = 0
 	end
+
 
 	def start
 		player_no = 1
 		begin
 			update(player_no)
+			@turn += 1
 			draw
-			# result = outcome
-			result = false
 			player_no =  player_no == 2? 1 : 2
+			result = outcome
 		end until(result == "done")
 	end
+
 
 	def update(player_no)
 		begin
@@ -54,6 +50,7 @@ class Game
 		@board[row][column] = "X"  if player_no == 1
 		@board[row][column] = "O"  if player_no == 2
 	end
+
 
 	def input_check(input)
 		row, column = input.split(" ").collect(&:to_i)
@@ -72,14 +69,50 @@ class Game
 		end
 	end
 
+
 	def outcome
-		# ...
-		# return result
+		if (@turn == 9)
+			puts "Draw"
+			return "done"
+		end
+
+		winner = nil
+		result = nil
+		@board.each do |row|
+			if (row[0] != " " && row[0] == row[1] && row[1] == row[2])
+				result = "done"
+				winner = row[0]
+				break
+			end
+		end
+
+		(0...3).each do |i|
+			if (@board[0][i] != " " && @board[0][i] == @board[1][i] && @board[2][i] == @board[1][i])
+				result = "done"
+				winner = @board[0][i]
+				break
+			end
+		end
+
+		if (@board[1][1] != " " && @board[0][0] == @board[1][1] && @board[1][1] == @board[2][2])
+			result = "done"
+			winner = @board[1][1]
+		end
+
+		if (@board[1][1] != " " && @board[0][2] == @board[1][1] && @board[1][1] == @board[2][0])
+			result = "done"
+			winner = @board[1][1]
+		end
+
+		puts "#{winner} Wins!"  if (result)
+		return result
 	end
+
 
 	def draw
 		puts `clear`
 		@board.each do |row|
+			print " "
 			puts row * " | "
 			puts
 		end
